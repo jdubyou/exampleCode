3d03b806c4c4a7d79017fad5d5c153ef52650134
Chris Aguero <chrismatthew176@yahoo.com>
2017-10-01 17:37:05 -0700

    Fixed problem

--- IsisPatty.js
+++ IsisPatty.js
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
 console.log("We are up in this bitch called StartBucks");
 console.log("Chris was here!");
-=======
 console.log("We are up in this bitch called StartBuckssssssssss");
->>>>>>> a72f2578cae3fb5a7be232c3661736dbd3ea24d4
